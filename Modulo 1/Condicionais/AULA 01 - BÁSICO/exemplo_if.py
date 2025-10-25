06dcba82f145e677c5dc1d3b410dbe28b13cf668
Guilherme Hermenegildo <guilhermehermenegildo2@gmail.com>
2025-10-25 10:55:40 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/AULA 01 - BÁSICO/exemplo_if.py
@@ -0,0 +1,16 @@
+
+
+idade = 16
+
+if idade <= 2:
+    print ('Bebê')
+elif idade <= 10:
+    print ('Criança')
+elif idade <= 14:
+    print ('Pré - Adolescente')
+elif idade <= 17:
+    print ('Adolescente')
+elif idade <= 60:
+    print ('Adulto')
+else:
+    print ('Idoso')
